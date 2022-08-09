4e40747c874ba4753ecf31bd5013e7e18e7b3773
Kirill Kuznetsov <shapowgorillalord@gmail.com>
2022-08-09 05:10:41 +0300

    inclination method added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def inclination(count, entity)
+    if (10..20).include?(count % 100)
+      entity[2]
+    else
+      case count % 10
+      when 1 then entity[0]
+      when 2..4 then entity[1]
+      else
+        entity[2]
+      end
+    end
+  end
 end
