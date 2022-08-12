01b2fdc0a6d8f655269e24a4a5f4cebc0d508e55
Kirill Kuznetsov <shapowgorillalord@gmail.com>
2022-08-12 16:31:46 +0300

    feature autologin after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
     user_params = params.require(:user).permit(:name, :nickname, :email, :password)
     @user = User.create(user_params)
 
+    session[:user_id] = @user.id
+
     redirect_to root_path, notice: 'Вы успешно зарегистрировались'
   end
 
