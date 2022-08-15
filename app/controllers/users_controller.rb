bdc19529aeb828ff163c214268c67f73eb16f31f
Kirill Kuznetsov <shapowgorillalord@gmail.com>
2022-08-15 18:31:01 +0300

    users now can be edited

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation)
     @user = User.create(user_params)
 
     if @user.save
@@ -16,5 +15,37 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Данные пользователя обновлены"
+    else
+      flash.now[:alert] = "При попытке сохранить данные пользователя возникли ошибки"
+
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    session.delete(:user_id)
+
+    redirect_to root_path, notice: "Пользователь удалён"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
-  get 'sessions/new'
   root to: "questions#index"
 
   resources :questions do
     put "hide", on: :member
   end
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit update destroy]
   resource :session, only: %i[new create destroy]
 end
