d2fc8237ba8f067684d85bff689528a6504a361f
Kirill Kuznetsov <shapowgorillalord@gmail.com>
2022-08-16 14:59:55 +0300

    validation fix(database_validations gem used)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def create
-    @user = User.create(user_params)
+    @user = User.new(user_params)
 
     if @user.save
       session[:user_id] = @user.id
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,20 @@
 class User < ApplicationRecord
   has_secure_password
 
-  before_save :downcase_nickname
+  before_validation :downcase_nickname
 
   validates :email,
-    presence: true,
-    uniqueness: true,
+    db_presence: true,
+    db_uniqueness: true,
     format: { with: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
 
   validates :nickname,
-    presence: true,
-    uniqueness: true,
-    format: { with: /\A\w+\z/, message: " can contain only Latin letters, numbers, and a sign _" },
+    db_presence: true,
+    db_uniqueness: true,
+    format: { with: /\A\w+\z/ },
     length: { maximum: 40 }
+
+  private
 
   def downcase_nickname
     nickname.downcase!
