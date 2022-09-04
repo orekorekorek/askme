d9819195b94bcfafbf51e291da4b195d11bf65e1
Kirill Kuznetsov <shapowgorillalord@gmail.com>
2022-09-04 21:36:10 +0300

    hotfix

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,5 @@
 module QuestionsHelper
   def text_with_hashtag_link(text)
-    strip_tags(text).gsub(Question::HASHTAG_REGEXP) { |ht| link_to ht, hashtag_path(ht.delete('#')).downcase }
+    strip_tags(text).gsub(Question::HASHTAG_REGEXP) { |ht| link_to ht, hashtag_path(ht.delete('#').downcase) }
   end
 end
